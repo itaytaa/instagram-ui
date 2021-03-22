8db1f59cd980559210f1b485e4aebd5becf6bdcf
itay taasa <itaytaasa@gmail.com>
2021-03-22 19:37:37 +0200

    heroku deploy

--- src/environments/index.js
+++ src/environments/index.js
@@ -1,3 +1,10 @@
-import { environment } from './development';
+import { development } from './development';
+import { production } from './production';
+
+
+let environment = development;
+if (process.env.NODE_ENV === 'production') {
+    environment = production;
+}
 
 export default environment;
