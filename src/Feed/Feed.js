9f00d576ae308dd85a21f372f2b3b74f1ba6c98c
itay taasa <itaytaasa@gmail.com>
2021-04-01 14:10:15 +0300

    feed message

--- src/Feed/Feed.js
+++ src/Feed/Feed.js
@@ -20,8 +20,18 @@ function Feed() {
 
     return (
         <div className="Feed  px-lg-5">
-            {posts.map((post)=><Post key={post._id} post={post} />
-              
+            {posts.length === 0 && <div className="feed-note">If your'e new here , Welcome! <br />
+            You can upload photos by clicking on the + button,<br />
+            Search for people to follow by clicking the search icon<br />
+             (Look for me by the name "itayT" and follow me!)<br />
+            Edit your User by clicking "Edit user" <br />
+            with the green button
+            </div>}
+
+
+
+            {posts.map((post) => <Post key={post._id} post={post} />
+
             )}
 
         </div>
